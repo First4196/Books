0faf4b704a9532928c47bd378c457c6f031bbb8c
Thanapat Katapornsiri <first-4196@hotmail.com>
2018-09-03 15:50:15 +0700

    2018-09-03

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript', pages: 105 },
+    { title: 'Python 101', pages: 101 },
+    { title: 'Harry Potter', pages: 1050 },
+    { title: 'Batman Return', pages: 232 }
+  ];
+}
